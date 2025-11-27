71d214a3700ac31139dbd560f993369ba6547f4a
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-27 11:05:32 +0530

    WOKING ON USESATE ON COMPONENTS

--- src/functionalDemo/FunctionalDemo.jsx
+++ src/functionalDemo/FunctionalDemo.jsx
@@ -1,17 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FunctionalDemo.css";
 
 function FunctionalDemo() {
-  let state = {
-    counter: 0,
-  };
+  const [counter, setCounter] = useState(0);
   function increaseValue() {
-    state.counter++;
-    console.log(`Value of the counter is :${state.counter}`);
+    setCounter(counter + 1);
+    console.log(`Value of the counter is : ${counter}`);
   }
   return (
     <div className="heading">
-      <h1>Counter: {state.counter}</h1>
+      <h1>Counter: {counter}</h1>
       <button className="brown-pill" onClick={increaseValue}>
         Increase
       </button>
