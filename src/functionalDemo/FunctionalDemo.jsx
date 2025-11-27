f2aaaae84b09e11a3e1d06376ea477d47006ff22
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-27 10:56:50 +0530

    STARTED HOOKS WITHA DUMMY STATE VARIABLE

--- src/functionalDemo/FunctionalDemo.jsx
+++ src/functionalDemo/FunctionalDemo.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import './FunctionalDemo.css'
+import "./FunctionalDemo.css";
 
-export default function FunctionalDemo() {
+function FunctionalDemo() {
+  let state = {
+    counter: 0,
+  };
+  function increaseValue() {
+    state.counter++;
+    console.log(`Value of the counter is :${state.counter}`);
+  }
   return (
     <div className="heading">
-      <h1>FunctionalDemo</h1>
+      <h1>Counter: {state.counter}</h1>
+      <button className="brown-pill" onClick={increaseValue}>
+        Increase
+      </button>
     </div>
   );
 }
+
+export default FunctionalDemo;
