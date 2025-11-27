ac954224d3ecc15ed3e56c8498fdcdcd67d4097e
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-27 11:12:38 +0530

    CREATED THREE BUTTONS AND MADE FUNCTIONING

--- src/functionalDemo/FunctionalDemo.jsx
+++ src/functionalDemo/FunctionalDemo.jsx
@@ -3,15 +3,34 @@ import "./FunctionalDemo.css";
 
 function FunctionalDemo() {
   const [counter, setCounter] = useState(0);
-  function increaseValue() {
-    setCounter(counter + 1);
-    console.log(`Value of the counter is : ${counter}`);
-  }
+  
   return (
     <div className="heading">
       <h1>Counter: {counter}</h1>
-      <button className="brown-pill" onClick={increaseValue}>
+      <button
+        className="brown-pill"
+        onClick={() => {
+          setCounter(counter + 1);
+          console.log(`Value of counter is : ${counter}`);
+        }}
+      >
         Increase
+      </button>
+      <button
+        className="green-pill"
+        onClick={() => {
+          setCounter(counter - 1);
+          console.log(`Value of the counter is : ${counter}`);
+        }}
+      >
+        Decrease
+      </button>
+      <button className="blue-pill" onClick={()=>{
+        setCounter(0);
+        console.log(`Value of the Counter is :${counter}`);
+        
+      }}>
+        Reset
       </button>
     </div>
   );
